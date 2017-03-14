32db0e8124aaaa2b4537ad983c28032d2df878e9
Maxime Labelle <maxime.labelle@moskitos.fr>
2017-03-14 16:17:50 +0100

    New - Added unit tests for current_node expressions.

---
+++ tests/jmespathnet.tests/Parser/CurrentNodeTest.cs
@@ -0,0 +1,15 @@
+
+namespace jmespath.net.tests.Parser
+{
+    using FactAttribute = Xunit.FactAttribute ;
+
+    public class CurrentNodeTest : ParserTestBase
+    {
+        [Fact]
+        public void ParseCurrentNode()
+        {
+            Assert("people[*].{a:@.age}", "{\"people\": [{\"age\": 20,\"other\": \"foo\",\"name\": \"Bob\"},{\"age\": 25,\"other\": \"bar\",\"name\": \"Fred\"},{\"age\": 30,\"other\": \"baz\",\"name\": \"George\"}]}", "[{\"a\":20},{\"a\":25},{\"a\":30}]");
+            Assert("[*].{a:@.age}", "[{\"age\": 20,\"other\": \"foo\",\"name\": \"Bob\"},{\"age\": 25,\"other\": \"bar\",\"name\": \"Fred\"},{\"age\": 30,\"other\": \"baz\",\"name\": \"George\"}]", "[{\"a\":20},{\"a\":25},{\"a\":30}]");
+        }
+    }
+}
