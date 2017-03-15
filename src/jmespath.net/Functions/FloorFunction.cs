56af866edfbfb6049b183a5cf03d896ee517f2b1
jdevillard <jeremie.devillard@moskitos.fr>
2017-03-15 19:14:09 +0100

    New - add support to floor function

---
+++ src/jmespath.net/Functions/FloorFunction.cs
@@ -0,0 +1,30 @@
+using System;
+using DevLab.JmesPath.Interop;
+using Newtonsoft.Json.Linq;
+
+namespace DevLab.JmesPath.Functions
+{
+    public class FloorFunction : JmesPathFunction
+    {
+        public FloorFunction()
+            : base("floor", 1)
+        {
+
+        }
+        public override bool Validate(params JToken[] args)
+        {
+            var arg = args[0];
+            if (arg.Type == JTokenType.Integer || arg.Type == JTokenType.Float)
+                return true;
+            else
+                throw new Exception("invalid-type");
+        }
+
+        public override JToken Execute(params JToken[] args)
+        {
+            var token = args[0];
+
+            return new JValue(Convert.ToInt32(Math.Floor(token.Value<double>())));
+        }
+    }
+}
