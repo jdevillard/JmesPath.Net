a9c2d5a6839169e4010e01a552790fc38ff63f31
jdevillard <jeremie.devillard@moskitos.fr>
2017-03-17 00:47:26 +0100

    fix - avg/sum function only support array[number]

--- src/jmespath.net/Functions/SumFunction.cs
+++ src/jmespath.net/Functions/SumFunction.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Linq;
 using DevLab.JmesPath.Expressions;
 using Newtonsoft.Json.Linq;
+using JmesPathFunction = DevLab.JmesPath.Interop.JmesPathFunction;
 
 namespace DevLab.JmesPath.Functions
 {
-    public class SumFunction : MathArrayFunction
+    public class SumFunction : JmesPathFunction
     {
         public SumFunction()
             : base("sum", 1)
         {
 
+        }
+
+        public override bool Validate(params JmesPathArgument[] args)
+        {
+            if (args[0].AsJToken().Type != JTokenType.Array)
+                throw new Exception("invalid-type");
+
+            foreach (var item in (JArray)args[0].AsJToken())
+                if (item.Type != JTokenType.Integer
+                    && item.Type != JTokenType.Float)
+                    throw new Exception("invalid-type");
+
+            return true;
         }
 
         public override JToken Execute(params JmesPathArgument[] args)
