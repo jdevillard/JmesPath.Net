07cae9cbb002d6825d5e7afef3e5af28afe534c6
jdevillard <jeremie.devillard@moskitos.fr>
2017-03-18 01:57:31 +0100

    remove assembly version in AssemblyInfo (seems to create duplicate with appveyor assembly patching

--- src/jmespath.net.signed/Properties/AssemblyInfo.cs
+++ src/jmespath.net.signed/Properties/AssemblyInfo.cs
@@ -17,6 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c1b8ba55-2115-406f-944d-39efa384879d")]
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
