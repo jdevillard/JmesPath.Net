f3181c87985f96b175384e92c1f2c6524e3cd7c7
Maxime Labelle <maxime.labelle@moskitos.fr>
2018-12-10 13:33:57 +0100

    Refactor - Upgraded project to Visual Studio 2017 and target .net standard 2.0.

--- src/jmespath.net.signed/update-version.ps1
+++ src/jmespath.net.signed/update-version.ps1
@@ -1,4 +1,10 @@
 Function Get-ScriptDirectory { Split-Path -parent $PSCommandPath }
 
-Write-Host $env:appveyor_build_version
-Set-Content -Path "$(Get-ScriptDirectory)\project.json" -Value (Get-Content "$(Get-ScriptDirectory)\project.json").Replace("`"version`": `"42.43.44`",", "`"version`": `"$($env:appveyor_build_version)`",")
+$newVersion = $env:appveyor_build_version
+
+Write-Host $newVersion
+
+$project = "$(Get-ScriptDirectory)\jmespath.net.signed.csproj"
+[xml]$document = Get-Content -Path $project -Raw
+$document.Project.PropertyGroup.VersionPrefix = $document.Project.PropertyGroup.VersionPrefix.Replace("42.43.44", $newVersion)
+$document.Save($project)
