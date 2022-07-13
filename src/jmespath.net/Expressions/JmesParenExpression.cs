6d69bb46666a8961edcfd1428971e4c1d053c5d5
Maxime Labelle <springcomp@users.noreply.github.com>
2022-07-13 19:56:23 +0200

    Extract `JmesPathParenExpression` in its own file.

---
+++ src/jmespath.net/Expressions/JmesParenExpression.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace DevLab.JmesPath.Expressions
+{
+    public class JmesParenExpression : JmesPathExpression
+    {
+        private readonly JmesPathExpression expression_;
+
+        public JmesParenExpression(JmesPathExpression expression)
+        {
+            expression_ = expression;
+        }
+
+        protected override JmesPathArgument Transform(JToken json)
+            => expression_.Transform(json);
+    }
+}
