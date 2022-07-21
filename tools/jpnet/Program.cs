ee103a3e76fb185afbed17cbfa9f182c42c8463c
Maxime Labelle <springcomp@users.noreply.github.com>
2022-07-21 21:30:20 +0200

    .NET runner for jp-compliance. (#61)

---
+++ tools/jpnet/Program.cs
@@ -0,0 +1,19 @@
+﻿using DevLab.JmesPath;
+
+if (args.Length == 0)
+{
+    Console.Error.WriteLine("Missing required JMESPath expression.");
+    Environment.Exit(42);
+}
+
+var expression = args[0];
+
+var stdin = Console.In;
+var stdout = Console.Out;
+
+var text = stdin.ReadToEnd();
+
+var jp = new JmesPath();
+var token = jp.Transform(text, expression);
+
+stdout.WriteLine(token);
